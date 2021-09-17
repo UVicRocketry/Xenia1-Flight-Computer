8c07f464380983b2f81f4a253b91623785a4191a
Josh Berryman <jberryman566@gmail.com>
2021-09-17 12:15:02 -0700

    Added functionality for lsm sensor

--- gpioReader.py
+++ gpioReader.py
@@ -3,6 +3,7 @@
 from time import sleep
 import RPi.GPIO as GPIO
 import board
+import adafruit_lsm9ds1
 from adafruit_bme280 import basic as adafruit_bme280
 
 # research needed
@@ -11,13 +12,22 @@ from adafruit_bme280 import basic as adafruit_bme280
 class GPIOReader():
 
     __bme280 = None
+    __lsm9ds1 = None
 
     def __readBME280(self):
         ## TODO: Store Values in CSV file for external use ##
         print("Temperature: %0.1f C" % __bme280.temperature)
         print("Humidity: %0.1f %%" % __bme280.humidity)
         print("Pressure: %0.1f hPa" % __bme280.pressure)
+
     
+    def __readLSM9DS1(self):
+        ## TODO: Store Values in CSV file for external use ##
+        print("Acceleration (m/s^2): ({0:0.3f}, {1:0.3f}, {2:0.3f})".format(*__lsm9ds1.Acceleration)
+        print("Magnetometer (gauss: ({0:0.3f}, {1:0.3f}, {2:0.3f})".format(*__lsm9ds1.Magnetometer)
+        print("Gyroscope (degrees/sec): ({0:0.3f}, {1:0.3f}, {2:0.3f})".format(*__lsm9ds1.Gyroscope)
+        print("Temperature: {0:0.3f}".format(__lsm9ds1.Temperature)
+
 
     ## Private Method for setting up variables and inputs on pins
     def __setup(self):
@@ -28,6 +38,7 @@ class GPIOReader():
 
         ## SETUP BME 280 SENSOR
         __bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+        __lsm9ds1 = adafruit_lsm9ds1.LSM9DS1_I2C(i2c)
 
     ## Public Method to be called externally to grab data from sensors
     def retrieveData(self):
