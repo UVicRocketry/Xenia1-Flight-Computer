bf911fb2cceb6ea945679dcc342b801fa6c6eb93
Kris Lemieux <45109638+klemie@users.noreply.github.com>
2022-02-07 11:52:03 -0800

    Update dataHandler.py

--- src/dataHandler.py
+++ src/dataHandler.py
@@ -5,16 +5,14 @@ class SendData:
     def __init__(self):
         rocket_data = rd()
     
-    def send_bme():
+    def send_bme(self):
         return
 
-    def send_imu():
+    def send_imu(self):
         return
 
-    def send_strain_gauges():
+    def send_strain_gauges(self):
         return
 
-    def convert_to_csv():
-        return
     
 
