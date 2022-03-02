a10b7fc6a9f9172edbf51c9d210e8a34b4ba9a24
JJ De Rooy <jjderooy123@gmail.com>
2022-03-01 17:24:30 -0800

    New branch to add stepper control

--- src/airbrakes.py
+++ src/airbrakes.py
@@ -3,4 +3,8 @@
 class KalmanFilter:
   def __init__(self, rd):
     rocket_data = rd
-    
+
+class Airbrakes:
+  def __init__(self, stepper_pin, direction_pin):
+    step_pin = stepper_pin
+    dir_pin  = direction_pin
