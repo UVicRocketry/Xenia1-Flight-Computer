0239369286f31caf70ee073830e5af0cd173e5df
jenny <jluu@uvic.ca>
2022-04-06 18:02:29 -0700

    CSV function in RocketData.py is done

--- src/rocketData.py
+++ src/rocketData.py
@@ -15,30 +15,30 @@ class RocketData:
     Methods
     -------
     Each attribute of the data object has a getter and setter denoted by @property by getter and @attribute.setter for setter
-    
+
     How to call a setter and getter:
     rocket_data = RocketData()
     rocket_data.time_stamp <- this gets the valus of time stamp
     rocket_data.time_stamp = 6.0 <- this Sets the value of time stamp to 6.0
-    
+
     multi variable sets and set through lists:
 
     rocket_data.imu = [5.0,6.0,7.0] <- sets in order of temperature, humidity, pressure see definitions of setters for more info
 
     for setting all data use rocket_data = [imu_t, imu_h, imu_p, bme_y, bme_p, bme_r, bme_a, bme_v, sg_1, sg_2, sg_3, sg_4, sg_5, sg_6, sg_7, sg_8, sg_9, sg_10, sg_11, sg_12, e, ts]
-    
+
     def data_dict_set(self, rd)
         Description:
-            updates the self._data with a dictionary. Differnet from setters 
-            because it does it updates the whole object not just individual 
-            attributes 
+            updates the self._data with a dictionary. Differnet from setters
+            because it does it updates the whole object not just individual
+            attributes
         Parm:
-            rd: updated _data object. 
+            rd: updated _data object.
 
-    data_to_json() 
-        Desciption: 
+    data_to_json()
+        Desciption:
             Converts the dictionary to a Json format
-    
+
     print_json_data()
         Desciption:
             prints the current state of the json to the terminal
@@ -78,11 +78,11 @@ class RocketData:
             'time_stamp': float
         }
 
-    # @property decorator makes the function below a getter 
+    # @property decorator makes the function below a getter
     @property
     def data(self):
         return self._data
-    
+
     @property
     def imu(self):
         return self._data['imu']
@@ -115,10 +115,10 @@ class RocketData:
     @property
     def strain_gauges(self):
         return self._data['strain_gauges']
-    
+
     @strain_gauges.setter
     def strain_gauges(self, vals):
-        one, two, three, four, five, six, seven, eight, nine, ten, evelen, twelve = vals
+        one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve = vals
         self._data['strain_gauges'][1] = one
         self._data['strain_gauges'][2] = two
         self._data['strain_gauges'][3] = three
@@ -126,16 +126,16 @@ class RocketData:
         self._data['strain_gauges'][5] = five
         self._data['strain_gauges'][6] = six
         self._data['strain_gauges'][7] = seven
-        self._data['strain_gauges'][8] = eight 
+        self._data['strain_gauges'][8] = eight
         self._data['strain_gauges'][9] = nine
         self._data['strain_gauges'][10] = ten
-        self._data['strain_gauges'][11] = evelen
+        self._data['strain_gauges'][11] = eleven
         self._data['strain_gauges'][12] = twelve
 
     @property
     def encoders(self):
         return self._data['encoders']['position']
-    
+
     @encoders.setter
     def encoders(self, p):
         self._data['encoders']['position'] = p
@@ -147,7 +147,7 @@ class RocketData:
     @time_stamp.setter
     def time_stamp(self, ts):
         self._data['time_stamp'] = ts
-    
+
     #Set all sensor data
     @data.setter
     def set_data(self, vals):
@@ -158,21 +158,21 @@ class RocketData:
         self.strain_gauges([sg_1, sg_2, sg_3, sg_4, sg_5, sg_6, sg_7, sg_8, sg_9, sg_10, sg_11, sg_12])
         self.encoders(e)
         self.time_stamp(ts)
-    
+
     def data_dict_set(self, rd):
         self._data.update(rd)
-        
+
     def data_to_json(self):
         rocketData = json.dumps(self._data, indent = 4)
         print(rocketData)
-        return rocketData 
-    
+        return rocketData
+
     def print_json_data(self):
         jsonData = self.data_to_json(self._data)
         print(jsonData)
-    
-    def convert_to_csv(self)
-        dataToConvert = [
+
+    def all_rocket_data(self):
+        allRocketData = [
             self._data['imu']['temperature'],
             self._data['imu']['humidity'],
             self._data['imu']['pressure'],
@@ -196,7 +196,16 @@ class RocketData:
             self._data['encoders']['position'],
             self._data['time_stamp']
         ]
-        return dataToConvert
+        return allRocketData
+
+    def convert_to_csv_string(self):
+        dToConvert = self.all_rocket_data()
+        csvString = str(dToConvert[0])
+        for d in dToConvert[1:]:
+            csvString += "," + str(d)
+
+        return csvString
+
     #TODO: convert json data to format that antenna needs
-    def convert_antenna_format(self)
+    def convert_antenna_format(self):
         jsonData = self.data_to_json()
