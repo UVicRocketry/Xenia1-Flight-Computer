a8ea3fc511a1fa7ef59897ecdfd7c0ef505060f1
macsyd <91709747+macsyd@users.noreply.github.com>
2022-05-11 16:40:59 -0700

    Update src/dataHandler.py
    Co-authored-by: Kris Lemieux <45109638+klemie@users.noreply.github.com>

--- src/dataHandler.py
+++ src/dataHandler.py
@@ -66,7 +66,7 @@ class SendData:
     
     def send_all_data(self, sendingTo, destination):
         if sendingTo == 'blackbox':
-            data = self.convert_to_csv()
+            data = self.rocket_data.convert_to_csv()
             f = open(destination, 'a')
             writer = csv.writer(f)
             writer.writerow(data)
