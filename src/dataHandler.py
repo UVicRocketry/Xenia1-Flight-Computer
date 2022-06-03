165f48e2b41fa3a65b15a1afcd8abc8f3310cd2c
Charlie Squires <cjsquires@users.noreply.github.com>
2022-06-03 15:19:28 -0700

    Update dataHandler.py
    Renamed variable names to more descriptive names

--- src/dataHandler.py
+++ src/dataHandler.py
@@ -60,58 +60,63 @@ class SendData:
             writer = csv.writer(f)
             writer.writerow(data)
             f.close()
-        
-    #gravity 
-    g = 9.80665
 
-    #gas_constant 
-    R = 8.3144598
+    #gravitational acceleration 
+    gravity = 9.80665
 
-    #atmo_molar_mass 
-    M = 0.289644
+    #gas constant
+    gas_const = 8.3144598
 
-    #H2O_vapourize_heat 
-    H_v = 2501000
+    #earth atmospheric molar mass
+    atmo_molar_mass = 0.289644
 
-    #spec_gas_const_dry 
-    R_sd = 287
+    #vapourization heat of water
+    H2O_vapour_heat = 2501000
+
+    #specific gas constant of dry air
+    specif_gas_const_dry = 287
     
-    #spec_gas_const_H2O 
-    R_sw = 461.5
+    #specific gas constant of water vapour
+    specif_gas_const_H2O = 461.5
     
-    #spec_heat_dry_air 
-    c_pd = 1003.5
+    #specific heat of dry air
+    specif_heat_dryair = 1003.5
 
-    #initialized_altitude
-    h_i = 274.1
+    #initialized altitude at launch pad
+    initialized_altitude = 274.1
     # just an estimate, actual data will be collected on site
 
-    prev_dh = 0
+    #previous calculated height change
+    prev_height_change = 0
     # previous change of height to compensate for Null case
 
+    #lapse rate of the air
+    lapse_rate = 0.0098
+    # this is the standard dry air lapse rate, altered by initializeLapseRate()
 
-    def initializeLapseRate(e, p, T):
+
+    def initializeLapseRate(moist, press, temp):
         """
         Takes moistness, pressure, and temperature readings at launch site to find lapse rate. 
         This should not be run after the initialization phase
 
         Paramaters:
-            e: float, moistness
-            p: float, pressure reading
-            T: float, temperature reading
+            moist: float, water vapour pressure
+            press: float, pressure reading
+            temp: float, temperature reading
 
         Returns:
             lapse_rate: float, lapse rate (... duh)
         """
-        numerator_numerator = H_v * e
-        numerator_denomenator = T * R_sw * (p - e)
+        numerator_numerator = H2O_vapour_heat * moist
+        numerator_denomenator = temp * specif_gas_const_H2O * (press - moist)
 
-        numerator = g * (1 + (numerator_numerator/numerator_denomenator))
+        numerator = gravity * (1 + (numerator_numerator/numerator_denomenator))
 
-        denomenator_numerator = H_v**2 * R_sd * e
-        denomenator_denomenator = (R_sw * T)**2 * (p - e)
+        denomenator_numerator = H2O_vapour_heat**2 * specif_gas_const_dry * moist
+        denomenator_denomenator = (specif_gas_const_H2O * temp)**2 * (press - moist)
 
-        denomenator = c_pd + (denomenator_numerator/denomenator_denomenator)
+        denomenator = specif_heat_dryair + (denomenator_numerator/denomenator_denomenator)
         
         lapse_rate = (numerator/denomenator)
         
@@ -121,96 +126,95 @@ class SendData:
 
 
 
-    def altitudeBarometric(p, p_i, T_i): # or altBaro, if needed
+    def altitudeBarometric(press, init_press, init_temp): # or altBaro, if needed
         """
         Takes pressure reading, initial pressure reading, and initial temperature reading to get altitude
 
         Parameters:
-            p: float, current pressure reading
-            p_i: float, initialized pressure reading, should not change after initialization
-            T_i: float, initialized temperature reading, should not change after initialization
+            press: float, current pressure reading
+            init_press: float, initialized pressure reading, should not change after initialization
+            init_temp: float, initialized temperature reading, should not change after initialization
 
         Returns:
-            alt_B: float, altitude from barometric pressure
+            alt_baro: float, altitude from barometric pressure
         """
 
-        if type(p) != None:
-            exponent = (-1 * R * L) / (g * M)
-            pressure_component = (p / p_i)**exponent
+        if type(press) != None:
+            exponent = (-1 * gas_const * lapse_rate) / (gravity * atmo_molar_mass)
+            pressure_component = (press / init_press)**exponent
 
-            alt_B = (pressure_component * T_i / L) + h_i - T_i
+            alt_baro = (pressure_component * init_temp / lapse_rate) + initialized_altitude - init_temp
 
-        elif type(p) == None:
-            alt_B = None
+        elif type(press) == None:
+            alt_baro = None
 
-        return alt_B
+        return alt_baro
 	
 
 
 
-    def altitudeTemperature_v1(T_c, T_p, prev_h): # or altTemp if needed
+    def altitudeTemperature_v1(curr_temp, prev_temp, prev_alt): # or altTemp if needed
         """
         Takes current temperature measurement, previous temperature measurement, 
         previous height, and previous height change to get altitude from temperature.
 
         Parameters:
-            T_c: float, current temperature reading
-            T_p: float, previous temperature reading
-            prev_h: float, previous height reading 
-                            (note: could either be previous height reading from this function or a previous accepted height reading)
+            curr_temp: float, current temperature reading
+            prev_temp: float, previous temperature reading
+            prev_alt: float, previous height reading 
+                      (note: could either be previous height reading from this function or a previous accepted height reading)
 
         Returns:
-            alt_T: float, altitude from temperature
-            prev_dh: new previous altitude change value
+            alt_temp: float, altitude from temperature
         """
 
         # NOTE: need a previous height global variable alongside the prev_dh to prevent compounding errors,
         # 			or a way to have the altitude reading be inputted as such
 
-        global prev_dh
+        global prev_height_change
 
-        if type(T_c) != None and type(T_p) != None and T_c < T_p:
-            dT = T_c - T_p
-            dh = -1 * (dT/L)
+        if type(curr_temp) != None and type(prev_temp) != None and curr_temp < prev_temp:
+            dT = curr_temp - prev_temp
+            dh = -1 * (dT/lapse_rate)
 
-            prev_dh = dh
+            prev_height_change = dh
 
-            alt_T = prev_h + dh
-            prev_h = alt_T
+            alt_temp = prev_alt + dh
+            prev_h = alt_temp
 
-        elif (type(T_c) == None or type(T_p) == None) and T_c < T_p:
-            dh = prev_dh
+        elif (type(curr_temp) == None or type(prev_temp) == None) and curr_temp < prev_temp:
+            dh = prev_height_change
 
-            alt_T = prev_h + dh
-            prev_h = alt_T
+            alt_temp = prev_alt + dh
+            prev_alt = alt_temp
 
         elif T_c > T_p:
-            alt_T = None
+            alt_temp = None
 
-            prev_h = prev_h + prev_dh
-
-
-        return alt_T
+            prev_alt = prev_alt + prev_height_change
 
 
+        return alt_temp
 
 
-    def altitudeTemperature_v2(T, T_i): # or altTemp if needed
+
+
+    def altitudeTemperature_v2(curr_temp, init_temp): # or altTemp if needed
         """
         Takes current temperature measurement and initialized temperature measurement change to get altitude from temperature.
-        This version essentially turns the flight path into two linear directions
+        This version essentially turns the flight path into two linear directions (possibly ignore this, docstrings to be changed)
 
         Parameters:
-            T: float, current temperature reading
-            T_i: float, initialized temperature, should not be altered after initialization phase
+            curr_temp: float, current temperature reading
+            init_temp: float, initialized temperature, should not be altered after initialization phase
 
         Returns:
-            alt_T: float, altitude from temperature
+            alt_temp: float, altitude from temperature
         """
 
-        alt_T = -1 * ((T - T_i)/L)
+        alt_temp = -1 * ((curr_temp - init_temp)/lapse_rate)
 
-        return alt_T
+        return alt_temp
     
     
     
