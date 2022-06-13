5799661d490fff7d2ef23edfecfedda46adf2227
JackCotter <79673714+JackCotter@users.noreply.github.com>
2022-06-13 15:08:53 -0700

    Update safe_value.py
    clarified comments, changed timeout to 5 sec because Aila said it somewhere

--- src/safe_value.py
+++ src/safe_value.py
@@ -1,23 +1,23 @@
 import time
+#time.time() returns the time from a specific date in seconds. I think the date is sometime in 1970. This is used to determine timeout.
 
 # TODO: Make into usable python in X1-AV-42. 
 #TIMEOUT is the amount of time in seconds until the last non None value be valid. After that the backup function will be used.
-TIMEOUT = 3
+TIMEOUT = 5
 
 # TODO: This is basically pseudo code. Would probably explode before the rocket
 class SafeValue:
     """Store a value from a sensor safely.
-    
-    This is only for values used in calculations. For values that
-    are only used in logging, just try else null."""
+    __last_value is for black box __last_safe_value is for calculations"""
 
     def __init__(self, safe_range, backup_function):
         # The last read value
         self.__last_value
-        # The last non-nil value
+        # The last non-nil value for calculations
         self.__last_safe_value
         # CPU timestamp to get delta t
         self.__last_safe_time
+        #range of values that are acceptable
         self.__safe_range = safe_range
         # Proc to backup to
         # TODO: Backup value needs some thought because different values 
@@ -26,7 +26,6 @@ class SafeValue:
         # These should be closures, procs, function pointers, or similar
         # Don't know what these are in Python -MC
         self.__backup_function = backup_function
-        #self.__read_function = read_function
     
     def __in_safe_range(self, value):
         """determines if a number is in __safe_range"""
@@ -35,7 +34,7 @@ class SafeValue:
     def update(self, value):
         """recieves a value updates __last_value with latest unsafe value (None type possible). If value is not None,
         __last_safe_value is updated"""
-        # Store the last read value blindly
+        # Store the last read value blindly in __last_value
         self.__last_value = value
 
         # If the value is in range and not None, store it safely
@@ -43,7 +42,7 @@ class SafeValue:
             self.__last_safe_value = value
             self.__last_safe_time = time.time()
 
-    # TODO: Bad name.
+    
     def get_last_safe_value(self):
         """Get the last safe value, or the backup if over time. If the last safe value is older than TIMEOUT then this function
         will return the backup Function. This is always called after update() so if the sensor comes back online, __last_safe_time
