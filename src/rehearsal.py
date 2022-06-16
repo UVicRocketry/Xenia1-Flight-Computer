9b5507cc15dd5e5dffd44ae0b0c7c0e534083105
Mateo Carreras <mateo.carreras@gmail.com>
2022-06-15 20:46:48 -0700

    add airbrakes rehearsal code and add constants to airbrakes.py

--- src/rehearsal.py
+++ src/rehearsal.py
@@ -6,6 +6,8 @@
 # MOST LOGIC SHOULD BE IN THE MAIN CODE. In theory, this should be only calling
 # functions and controlling the flow.
 
+from airbrakes import Airbrakes
+
 def startup():
     """Initialize all the things"""
     init_stepper()
@@ -20,7 +22,13 @@ def init_stepper():
     initialization.
 
     Don't stand next to the airbrakes at this point."""
-    pass
+
+    # TODO: I have ZERO idea what this direction should be. There is a 50%
+    #       chance that this is correct.
+    airbrakes = Airbrakes(direction = True)
+
+    # This will wave at the fans (move the brakes in and out)
+    airbrakes.calibrate()
 
 
 def tare_hx711s():
